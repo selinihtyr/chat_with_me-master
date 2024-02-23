7c420920a0c6a55938540788f3f1e127a5970296
Selin İhtiyar <nurselinihtiyar@gmail.com>
2024-02-23 12:20:01 +0300

    feat: Add UserRepository class implementing AuthBase interface

---
+++ lib/repository/user_repository.dart
@@ -0,0 +1,42 @@
+import 'package:chat_with_me/model/user_app.dart';
+import 'package:chat_with_me/services/auth_base.dart';
+import 'package:chat_with_me/services/fake_authentication_service.dart';
+import 'package:chat_with_me/services/firebase_auth_services.dart';
+import 'package:chat_with_me/services/locator.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+enum AppMode { DEBUG, RELEASE }
+
+FirebaseAuthServices _firebaseAuthServices = locator<FirebaseAuthServices>();
+FakeAuthenticationService _fakeAuthenticationService =
+    locator<FakeAuthenticationService>();
+AppMode appMode = AppMode.DEBUG;
+
+class UserRepository implements AuthBase {
+  @override
+  Future<AppUser?> currentUser() async {
+    if (appMode == AppMode.DEBUG)  {
+      return await _fakeAuthenticationService.currentUser();
+    } else {
+      return await _firebaseAuthServices.currentUser();
+    }
+  }
+
+  @override
+  Future<AppUser?> signInAnonymously() async{
+    if (appMode == AppMode.DEBUG) {
+      return await _fakeAuthenticationService.signInAnonymously();
+    } else {
+      return await _firebaseAuthServices.signInAnonymously();
+    }
+  }
+
+  @override
+  Future<bool> signOut() async {
+    if (appMode == AppMode.DEBUG) {
+      return await _fakeAuthenticationService.signOut();
+    } else {
+      return await _firebaseAuthServices.signOut();
+    }
+  }
+}
